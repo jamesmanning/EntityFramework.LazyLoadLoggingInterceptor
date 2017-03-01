f84c58c355f9cc6f381e968ae4263a431efd799e
James Manning <james.manning@gmail.com>
2017-03-01 01:04:13 -0500

    get an initial unit test up and going
    - currently just confirms we can do lazy-loading with a sqlce compact database
    - confirming these lazy-loads trip the interceptor and then explicit-loading them and confirming they don't will come next

---
+++ jmm.EntityFramework.Tests/LazyLoadLoggingInterceptorTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using Xunit;
+
+namespace jmm.EntityFramework.Tests
+{
+    public class LazyLoadLoggingInterceptorTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+            // Act
+            var customer = new Customer()
+            {
+                Name = "SomeCustomerName",
+                Invoices = new List<Invoice>()
+                {
+                    new Invoice()
+                    {
+                        Number = "SomeInvoiceNumber",
+                    }
+                },
+            };
+            using (var db = new CustomerDbContext())
+            {
+                db.Customers.Add(customer);
+                db.SaveChanges();
+
+                var customers = db.Customers.ToList();
+                Assert.NotNull(customers);
+                Assert.Equal(1, customers.Count);
+                Assert.Equal("SomeCustomerName", customers[0].Name);
+
+                // lazy load via navigation entity collection
+                Assert.NotNull(customers[0].Invoices);
+                Assert.Equal(1, customers[0].Invoices.Count);
+                Assert.Equal(1, customers[0].Invoices.Single().CustomerId);
+                Assert.Equal(1, customers[0].Invoices.Single().InvoiceId);
+                Assert.Equal("SomeInvoiceNumber", customers[0].Invoices.Single().Number);
+
+                var invoices = db.Invoices.ToList();
+                Assert.NotNull(invoices);
+                Assert.Equal(1, invoices.Count);
+                Assert.Equal("SomeInvoiceNumber", invoices[0].Number);
+
+                // lazy load via navigation entity reference
+                Assert.Equal(1, invoices[0].InvoiceId);
+                Assert.Equal(1, invoices[0].CustomerId);
+                Assert.NotNull(invoices[0].Customer);
+                Assert.Equal(1, invoices[0].Customer.CustomerId);
+                Assert.Equal("SomeCustomerName", invoices[0].Customer.Name);
+            }
+        }
+    }
+    public class CustomerDbContext : DbContext
+    {
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+    public class Customer
+    {
+        public int CustomerId { get; set; }
+        public string Name { get; set; }
+        public virtual ICollection<Invoice> Invoices { get; set; }
+    }
+    public class Invoice
+    {
+        public int InvoiceId { get; set; }
+        public string Number { get; set; }
+
+        public int CustomerId { get; set; }
+        public virtual Customer Customer { get; set; }
+    }
+}
